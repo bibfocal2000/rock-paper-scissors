7bc0cf190028c2395c4e6cb09d60c4d97c18ce2e
bibfocal2000 <mrbrese@gmail.com>
2022-03-23 13:43:47 +0800

    Create PlayRound function

--- main.js
+++ main.js
@@ -15,3 +15,34 @@ const computerPlay = () => {
 const playerPlay = (playerInput) => {
   return playerInput.toLowerCase();
 };
+
+//* Plays one round of the game by asking the player their choice with a prompt.
+//* Logic included to restict the player to rock, paper, or scissors
+//* Takes two functions as parameters, computerPlay() and playerPlay().
+//* Variables store the values provided by both functions and then passes those values into conditional logic to determine who wins the round. A win adds 1 to a score variable and then both players scores are evaluated
+//* Logic for a tie and a typo are also added
+const playRound = (computerPlay, playerPlay) => {
+  let playerInput = prompt("Enter either 'rock', 'paper', or 'scissors");
+  let computer = computerPlay();
+  let player = playerPlay(playerInput);
+
+  if (
+    (computer === "rock" && player === "scissors") ||
+    (computer === "paper" && player === "rock") ||
+    (computer === "scissors" && player === "paper")
+  ) {
+    computerScore++;
+    return `${computer} beats ${player}, computer wins this round`;
+  } else if (
+    (player === "rock" && computer === "scissors") ||
+    (player === "paper" && computer === "rock") ||
+    (player === "scissors" && computer === "paper")
+  ) {
+    playerScore++;
+    return `${player} beats ${computer}, player wins this round`;
+  } else if (computer === player) {
+    return `${player} = ${computer}, it's a tie`;
+  } else {
+    return `${playerInput} is not an option`;
+  }
+};
